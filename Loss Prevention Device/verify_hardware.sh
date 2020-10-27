76a89d370b5ecab230630152542e5de1fcce688b
Spencer Wong <Spwo@users.noreply.github.com>
2020-10-27 17:28:28 -0500

    Added hardware verification script

---
+++ Loss Prevention Device/verify_hardware.sh
@@ -0,0 +1,19 @@
+!#/bin/bash
+
+cd /sys/class/gpio/gpio43 #TX from bluetooth module
+cat direction
+cat value
+
+cd /sys/class/gpio/gpio42 #RX from bluetooth module
+cat direction
+cat value
+
+cd /sys/class/gpio/gpio31 #TX from GPS
+cat direction
+cat value
+
+cd /sys/class/gpio/gpio30 #RX from GPS
+cat direction
+cat value
+
+ls usb
