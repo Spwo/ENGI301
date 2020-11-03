b59fb7acd92f8e2eecf97415cbee8fe44c396a1d
Spencer Wong <Spwo@users.noreply.github.com>
2020-11-02 23:27:45 -0600

    New GPS datasheet and python code

---
+++ Loss Prevention Device/MainLPD.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+--------------------------------------------------------------------------
+LPD_Main
+--------------------------------------------------------------------------
+Authors: Spencer Wong (sbw1 [at] rice [dot] edu)
+ 
+Copyright 2020, Spencer Wong
+
+License:  
+
+Redistribution and use in source and binary forms, with or without 
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, 
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, 
+this list of conditions and the following disclaimer in the documentation 
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors 
+may be used to endorse or promote products derived from this software without 
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+THE POSSIBILITY OF SUCH DAMAGE.
+--------------------------------------------------------------------------
+
+Program that will be used for loss prevention device. See Code flow diagram.
+
+--------------------------------------------------------------------------
+"""
+
+
+
+"""The DSD TECH app shows about -50 dBm when VERY close to the GPS,
+and about -87 when 385 inches away"""
+
+"""I downloaded the DSD tech windows tool and was able to connect to it, 
+but idk what to do with that""
